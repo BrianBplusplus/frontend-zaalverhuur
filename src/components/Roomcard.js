82e256a71220923c7299f795de3f3cd568b88e60
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-07-20 16:44:46 +0200

    Finished basic styling

--- src/components/Roomcard.js
+++ src/components/Roomcard.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
+import image from "../img/dnbkamer.jpg";
 
 const bodyStyle = {
-  width: "25%",
-  border: "1px solid black",
+  width: "230px",
+  boxShadow: "0 0 0 1px rgba(0,0,0,.15), 0 2px 3px rgba(0,0,0,.2)",
   textAlign: "center",
   padding: "10px",
   margin: "10px 10px",
+};
+
+const imageStyle = {
+  maxWidth: "100%",
+};
+
+const buttonStyle = {
+  color: "#fff",
+  backgroundColor: "#ed008c",
+  border: "none",
+  padding: "20px",
 };
 
 export default class Roomcard extends Component {
   render() {
     return (
       <div style={bodyStyle}>
-        <img src={this.props.roomImage} />
+        <img style={imageStyle} src={image} />
         <h2>{this.props.title}</h2>
         <p>{this.props.description}</p>
         <ul>
@@ -20,7 +32,7 @@ export default class Roomcard extends Component {
           <li>{this.props.capacity}</li>
           <li>{this.props.setups}</li>
         </ul>
-        <button>Reserveren</button>
+        <button style={buttonStyle}>Reserveren</button>
       </div>
     );
   }
