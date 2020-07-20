edf87e03494389a0e33d4e5486da4bc73ecffb13
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-07-20 15:02:18 +0200

    Small header styling changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import logo from '../img/dnblogo.jpg' 
+import logo from "../img/dnblogo.jpg";
+
+const headerStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "10px",
+};
+
+const navStyle = {
+  display: "flex",
+  listStyle: "none",
+  margin: "0 10px",
+  padding: "0",
+};
+
+const listStyle = {
+  margin: "0 10px",
+};
 
 export default class header extends Component {
-    render() {
-        return (
-            <div>
-                <img src={logo} />
-                Vergaderruimtes inloggen Terug naar Bibliotheek
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={headerStyle}>
+        <img src={logo} />
+        <ul style={navStyle}>
+          <li style={listStyle}>Vergaderruimtes</li>
+          <li style={listStyle}>inloggen</li>
+          <li style={listStyle}>Terug naar Bibliotheek</li>
+        </ul>
+      </div>
+    );
+  }
 }
--- src/components/LandingPageContainer.js
+++ src/components/LandingPageContainer.js
@@ -6,13 +6,20 @@ export default class LandingPageContainer extends Component {
         return (
             <div>
                 <Roomcard 
-                    
                     title="De nieuwe zaal"
                     description="De nieuwe zaal is onze grootste zaal en is dé plek voor events!"
                     surfacearea="143 m2"
                     capacity="max. 112"
                     setups="Opstellingen"
                 />
+
+                <Roomcard
+                    title="De nieuwe zaal"
+                    description="De nieuwe zaal is onze grootste zaal en is dé plek voor events!"
+                    surfacearea="143 m2"
+                    capacity="max. 112"
+                    setups="Opstellingen" />
+
             </div>
         )
     }
