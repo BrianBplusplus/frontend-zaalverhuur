8aead315de6adce119cff2960068afd0fb6dd553
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-07-20 13:36:33 +0200

    Basic component layout implemented

--- src/components/LandingPageContainer.js
+++ src/components/LandingPageContainer.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import Roomcard from './Roomcard'
 
 export default class LandingPageContainer extends Component {
     render() {
         return (
             <div>
-                
+                <Roomcard 
+                    
+                    title="De nieuwe zaal"
+                    description="De nieuwe zaal is onze grootste zaal en is dé plek voor events!"
+                    surfacearea="143 m2"
+                    capacity="max. 112"
+                    setups="Opstellingen"
+                />
             </div>
         )
     }
 }
+
