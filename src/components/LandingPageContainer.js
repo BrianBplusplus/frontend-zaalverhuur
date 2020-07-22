102d758fad19e5251c2268cec65d0b8ff381ad7f
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-07-22 12:56:53 +0200

    Changed LandingPageContainer from a class based component to a functional component

--- src/components/LandingPageContainer.js
+++ src/components/LandingPageContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Roomcard from "./Roomcard";
 
@@ -11,42 +11,37 @@ const bodyStyle = {
   margin: "auto",
 };
 
-export default class LandingPageContainer extends Component {
-  state = {
-    titles: "",
-  };
+function LandingPageContainer() {
+  const [title, setTitles] = useState("");
 
-  async componentDidMount() {
-    try {
-      const response = await axios.get(
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await await axios.get(
         `https://jsonplaceholder.typicode.com/users`
       );
       console.log("API response", response);
-      const fetchedTitles = response.data;
-      this.setState({ titles: fetchedTitles });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+      setTitles(response.data);
+    };
+    fetchAPI();
+  }, []);
 
-  render() {
-    console.log("this.state", this.state);
-    return (
-      <div style={bodyStyle}>
-        {this.state.titles &&
-          this.state.titles.map((mappedTitle) => {
-            return (
-              <Roomcard
-                key={mappedTitle.id}
-                title={mappedTitle.name}
-                description="De nieuwe zaal is onze grootste zaal en is dé plek voor events!"
-                surfacearea="143 m2"
-                capacity="max. 112"
-                setups="opstellingen"
-              />
-            );
-          })}
-      </div>
-    );
-  }
+  return (
+    <div style={bodyStyle}>
+      {title &&
+        title.map((mappedTitle) => {
+          return (
+            <Roomcard
+              key={mappedTitle.id}
+              title={mappedTitle.name}
+              description="De nieuwe zaal is onze grootste zaal en is dé plek voor events!"
+              surfacearea="143 m2"
+              capacity="max. 112"
+              setups="opstellingen"
+            />
+          );
+        })}
+    </div>
+  );
 }
+
+export default LandingPageContainer;
