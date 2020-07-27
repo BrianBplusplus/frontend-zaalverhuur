110bcc005d84433e67db346a878c1173f1144520
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-07-27 12:10:19 +0200

    Updated the footer component to look more like the main website
    Still has mostly dummy content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,70 @@ import React from "react";
 
 const Footer = () => {
   const divStyle = {
-    left: "0",
-    right: "0",
-    bottom: "0",
-
+    display: "flex",
+    justifyContent: "space-between",
     color: "#fff",
     backgroundColor: "#000",
     padding: "20px 20px 40px 20px",
     margin: " 20px auto",
   };
 
-  return <div style={divStyle}>Footer Content</div>;
+  const titleStyle = {
+    fontSize: "1.3rem",
+    fontWeight: "normal",
+  };
+
+  const ulStyle = {
+    listStyle: "none",
+    padding: "0",
+  };
+
+  return (
+    <div style={divStyle}>
+      <div>
+        <h2 style={titleStyle}>KUNNEN WIJ JE HELPEN?</h2>
+        <ul style={ulStyle}>
+          <li>Verlengen</li>
+          <li>Reserveren</li>
+          <li>Regels PC gebruik</li>
+          <li>Printen</li>
+          <li>Veelgestelde vragen</li>
+        </ul>
+      </div>
+      <div>
+        <h2 style={titleStyle}>BLIJF OP DE HOOGTE!</h2>
+        <ul style={ulStyle}>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+          <li>Youtube</li>
+          <li>Nieuwsbrief</li>
+        </ul>
+      </div>
+      <div>
+        <h2 style={titleStyle}>INFORMATIEPUNTEN</h2>
+        <ul style={ulStyle}>
+          <li>ZEPservice</li>
+          <li>Seats2meet</li>
+          <li>Leeskringen</li>
+          <li>Europe-direct</li>
+          <li>Plusbibliotheek</li>
+        </ul>
+      </div>
+      <div>
+        <h2 style={titleStyle}>DENIEUWEBIBLIOTHEEK</h2>
+        <ul style={ulStyle}>
+          <li>Over denieuwebibliotheek</li>
+          <li>Werken bij de bibliotheek</li>
+          <li>Contact</li>
+          <li>Adressen</li>
+          <li>Rondleidingen</li>
+          <li>Jaarverslagen</li>
+          <li>Privacy</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
