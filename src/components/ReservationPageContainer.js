63c45e7fb82aba5350b3b6a58b6287da797c07ae
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-08-13 16:49:29 +0200

    Added states as a setup for room reservation

--- src/components/ReservationPageContainer.js
+++ src/components/ReservationPageContainer.js
@@ -16,6 +16,9 @@ const ReservationPageContainer = () => {
   const [isError, setIsError] = useState(false);
 
   const [pickedDate, setPickedDate] = useState(null);
+  const [pickedDayPart, setPickedDayPart] = useState(null);
+  const [pickedMeal, setPickedMeal] = useState(null);
+  const [pickedSeatPlan, setPickedSeatplan] = useState(null);
 
   // ---------------- Variables ---------------- //
   const params = useParams();
@@ -63,11 +66,6 @@ const ReservationPageContainer = () => {
     setPickedDate(selected ? undefined : day);
   };
 
-  const dateChecker = () => {
-    console.log(pickedDate);
-    console.log(typeof pickedDate);
-  };
-
   // ---------------- Styling ------------------ //
   const divStyle = {
     display: "flex",
@@ -76,6 +74,12 @@ const ReservationPageContainer = () => {
     minHeight: "30vh",
     backgroundColor: "#fff",
     padding: "15px",
+  };
+
+  const sidebarPStyle = {
+    textAlign: "center",
+    fontSize: "14px",
+    color: "#636363",
   };
 
   const leftStyle = {};
@@ -109,7 +113,23 @@ const ReservationPageContainer = () => {
         <h2>{apiData.name}</h2>
         <p>{descriptionData[paramsArrayIndex]}</p>
 
-        <h2>Opties : </h2>
+        <h2>Opties</h2>
+        <h3>Maaltijden</h3>
+        <ul>
+          <li onClick={() => setPickedMeal("Kleine lunch")}>
+            Kleine lunch €20,-
+          </li>
+          <li onClick={() => setPickedMeal("Warme maaltijd")}>
+            Warme maaltijd €50,-
+          </li>
+          <li onClick={() => setPickedMeal("Restaurant")}>Restaurant €100,-</li>
+        </ul>
+        <h3>Opstellingen</h3>
+        <ul>
+          <li onClick={() => setPickedSeatplan("U-vorm")}>U-vorm</li>
+          <li onClick={() => setPickedSeatplan("Klaslokaal")}>Klaslokaal</li>
+          <li onClick={() => setPickedSeatplan("Theater")}>Theater</li>
+        </ul>
         <Link to="/">
           <LargeButton text="Terug naar zalen" />
         </Link>
@@ -120,9 +140,21 @@ const ReservationPageContainer = () => {
           onDayClick={handlePickDate}
           disabledDays={{ daysOfWeek: [0] }}
         />
-
-        <p>Geselecteerde opties: </p>
-        <button onClick={() => dateChecker()}>Check datum</button>
+        <ul>
+          <li onClick={() => setPickedDayPart("Ochtend")}>Ochtend</li>
+          <li onClick={() => setPickedDayPart("Middag")}>Middag</li>
+          <li onClick={() => setPickedDayPart("Avond")}>Avond</li>
+        </ul>
+        <div>
+          <p style={sidebarPStyle}>Geselecteerde opties</p>
+          <ul>
+            <li>Dagdeel: {pickedDayPart}</li>
+            <li>Maaltijd: {pickedMeal}</li>
+            <li>Opstelling: {pickedSeatPlan}</li>
+          </ul>
+          <input type="email"></input>
+          <p>Op basis van uw selectie wordt een offerte gemaakt</p>
+        </div>
         <LargeButton text="Reserveren" />
       </div>
     </div>
