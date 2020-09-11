ddf3a886d3efadf28f7f2d132d43b64c60de7efd
Brian Nieuwenhuijzen <brian@nieuwenhuijzen.net>
2020-09-11 14:53:11 +0200

    Added a transition zoom to the image

--- src/components/DetailPage/DetailPageImage.js
+++ src/components/DetailPage/DetailPageImage.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
+
+import zoomIcon from "../../img/zoomIcon.svg";
 
 const DetailPageImage = (props) => {
   // ---------------- States ------------------- //
   const [zoomedImage, setZoomedImage] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+
+  // ---------------- Variables ---------------- //
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   // ---------------- Styling ------------------ //
   const imageStyle = {
@@ -11,14 +18,21 @@ const DetailPageImage = (props) => {
     width: "100%",
     height: "200px",
     objectFit: "cover",
+    opacity: isHover ? "0.6" : "1",
+    transition: "ease 0.2s",
   };
 
   const zoomedImageStyle = {
     display: zoomedImage ? "block" : "none",
     position: "fixed",
     zIndex: "2",
-    top: "75px",
-    width: "1200px",
+    width: "95%",
+    maxWidth: "1300px",
+    border: "1px solid #ed008c",
+
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
   };
 
   const zoomedImageBackground = {
@@ -35,18 +49,60 @@ const DetailPageImage = (props) => {
     opacity: "0.9",
   };
 
+  const svgStyle = {
+    width: "5%",
+    height: "auto",
+    position: "absolute",
+    opacity: isHover ? "1" : "0",
+    transition: "ease 0.2s",
+  };
+
   // ---------------- Render ------------------- //
   return (
     <div>
+      {isMobile && <img alt="LocationImage" style={imageStyle} src={props.image} />}
+
+      {!isMobile && (
+        <motion.img
+          alt="LocationImage"
+          style={imageStyle}
+          src={props.image}
+          onClick={() => setZoomedImage(true)}
+          onHoverStart={() => setIsHover(true)}
+          onHoverEnd={() => setIsHover(false)}
+        />
+      )}
       <motion.img
-        whileHover={{ opacity: 0.5 }}
-        alt="LocationImage"
-        style={imageStyle}
-        src={props.image}
+        style={svgStyle}
+        src={zoomIcon}
         onClick={() => setZoomedImage(true)}
+        onHoverStart={() => setIsHover(true)}
+        onHoverEnd={() => setIsHover(false)}
       />
-      <img style={zoomedImageStyle} src={props.image} onClick={() => setZoomedImage(false)} />
-      <div style={zoomedImageBackground} onClick={() => setZoomedImage(false)}></div>
+      <AnimatePresence>
+        {zoomedImage && (
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            style={zoomedImageStyle}
+            src={props.image}
+            onClick={() => setZoomedImage(false)}
+          />
+        )}
+      </AnimatePresence>
+
+      <AnimatePresence>
+        {zoomedImage && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            style={zoomedImageBackground}
+            onClick={() => setZoomedImage(false)}
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
